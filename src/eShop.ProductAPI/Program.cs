fe8e56fee07836adf3e50eff9ad7c0e7eb9ef817
Ricardo Fulgencio Alves <rfulgencio3@gmail.com>
2022-05-23 08:34:28 -0300

    Ajuste referência cíclica

--- src/eShop.ProductAPI/Program.cs
+++ src/eShop.ProductAPI/Program.cs
@@ -2,10 +2,13 @@ using eShop.ProductAPI.Context;
 using eShop.ProductAPI.Repository;
 using eShop.ProductAPI.Services;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
