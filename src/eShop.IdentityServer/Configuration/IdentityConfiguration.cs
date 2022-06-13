65d344e0a9dfc6959fd7e92d3404caf668460891
rfulgencio3 <rfulgencio3@gmail.com>
2022-06-13 16:30:31 -0300

    Configuração de projeto EntityServer

---
+++ src/eShop.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace eShop.IdentityServer.Configuration;
+public class IdentityConfiguration
+{
+    public const string Admin = "Admin";
+    public const string Client = "Client";
+
+    public static IEnumerable<IdentityResource> IdentityResources =>
+      new List<IdentityResource>
+      {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+      };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+      new List<ApiScope>
+      {
+                // eShop is the web application to access
+                // o IdentityServer to get token
+                new ApiScope("eShop", "eShop Server"),
+                new ApiScope(name: "read", "Read data."),
+                new ApiScope(name: "write", "Write data."),
+                new ApiScope(name: "delete", "Delete data."),
+      };
+
+    public static IEnumerable<Client> Clients =>
+       new List<Client>
+       {
+               //generic Client
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("abracadabra#simsalabim".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials, //need user credentials
+                    AllowedScopes = {"read", "write", "profile" }
+                },
+                new Client
+                {
+                    ClientId = "eShop",
+                    ClientSecrets = { new Secret("abracadabra#simsalabim".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code, //by code
+                    RedirectUris = {"https://localhost:7165/signin-oidc"},//login
+                    PostLogoutRedirectUris = {"https://localhost:7165/signout-callback-oidc"},//logout
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "eShop"
+                    }
+                }
+       };
+
+}
---
+++ src/eShop.IdentityServer/Program.cs
@@ -0,0 +1,58 @@
+using eShop.IdentityServer.Configuration;
+using eShop.IdentityServer.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+                                options.UseMySql(connection,
+                                ServerVersion.AutoDetect(connection)));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+var builderIdentityServer = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(
+                           IdentityConfiguration.IdentityResources)
+                            .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+                            .AddInMemoryClients(IdentityConfiguration.Clients)
+                            .AddAspNetIdentity<ApplicationUser>();
+
+builderIdentityServer.AddDeveloperSigningCredential();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseIdentityServer();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
