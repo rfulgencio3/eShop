6a2a726fb4d702b4176cfac1084b53bb731c3d78
rfulgencio3 <rfulgencio3@gmail.com>
2022-06-23 13:50:20 -0300

    Implementação de autenticação em projeto API + validação de JWT Token no Swagger

--- src/eShop.ProductAPI/Program.cs
+++ src/eShop.ProductAPI/Program.cs
@@ -2,6 +2,8 @@ using eShop.ProductAPI.Context;
 using eShop.ProductAPI.Repository;
 using eShop.ProductAPI.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,20 +15,73 @@ builder.Services.AddControllers().AddJsonOptions(x =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//Usign JWT with Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "eShop.ProductAPI", Version = "v1" });
 
-var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] your token",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string> ()
+        }
+    });
+});
+
+var connection = builder.Configuration
+    .GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-            options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
+            options.UseMySql(connection, 
+                ServerVersion.AutoDetect(connection)));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority =
+            builder.Configuration["eShop.IdentitySercer:ApplicationUrl"];
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "eShop");
+    });
+});
 
 var app = builder.Build();
 
@@ -39,6 +94,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
